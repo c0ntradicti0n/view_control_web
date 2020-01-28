03b3b9797803533841722f5fd3662f52be77e998
SW <dagedieb@gmail.com>
2020-01-28 13:40:37 +0100

    legal mark for private website

---
+++ src/me/cc/beans/CorpusBean.java
@@ -0,0 +1,29 @@
+package me.cc.beans;
+
+import me.cc.model.AnyAnswer;
+import me.cc.model.Distinction;
+import me.cc.model.Tag;
+import me.cc.model.annotationTagsFactory;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+@ViewScoped
+@ManagedBean(name = "commandBean")
+public class CorpusBean implements Serializable {
+	CcPyBean ccPyBean;
+	public ArrayList<ArrayList<Distinction>> Distinctions;
+
+	@PostConstruct
+	public void init() {
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		ccPyBean = (CcPyBean) facesContext.getApplication().createValueBinding("#{ccPyBean}").getValue(facesContext);
+	}
+
+
+
+}
